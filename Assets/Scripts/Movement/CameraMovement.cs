afaf67d1a1a88977c90083b1a3d8af81f9583905
AnnKryukova <48625386+AnnKryukova@users.noreply.github.com>
2020-10-14 13:48:33 +0500

    Добавлено движение игрока и камеры

---
+++ Assets/Scripts/Movement/CameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Класс отвечает за плавное движение камеры
+/// </summary>
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject player;
+    [SerializeField]
+    private float smoothing = 0.05f;
+
+    void Start()
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+
+    private void LateUpdate()
+    {
+        if(transform.position != player.transform.position)
+        {
+            Vector3 playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, playerPosition, smoothing);
+        }
+    }
+}

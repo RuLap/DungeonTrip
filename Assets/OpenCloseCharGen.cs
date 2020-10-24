78321f424875a3892d60261ccc435a9fc5bf9261
AdmiralSlowpoke <maksimzlt@gmail.com>
2020-10-24 13:15:06 +0500

    Фиксы переноса

--- Assets/OpenCloseCharGen.cs
+++ Assets/OpenCloseCharGen.cs
@@ -1,14 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class OpenCloseCharGen : MonoBehaviour
 {
     // Start is called before the first frame update
     Animator anim;
+    private bool state = false;
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            if (!state)
+            {
+                clickOnOn();
+            }
+            else
+                clickOnOff();
+            state = !state;
+        }
     }
     public void clickOnOn()
     {

b18badd5497907a5000299b66212a266feaa8870
Igor Meshkov <meshkov-74@yandex.ru>
2020-10-31 17:59:46 +0300

    Добавлен экран загрузки и canvas в префабы

---
+++ Assets/Scripts/LoadScreen.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadScreen : MonoBehaviour
+{
+    private int progress = 0;
+    private Image bar;
+
+    private void Start()
+    {
+        bar = GetComponent<Image>();
+        StartCoroutine(Load());
+    }
+    private void Update()
+    {
+        if(progress == 100)
+        {
+            SceneManager.LoadScene(PlayerPrefs.GetString("Scene"));
+        }
+    }
+    IEnumerator Load()
+    {
+        while (true)
+        {
+            progress++;
+            bar.fillAmount = progress / 100.0f;
+            yield return new WaitForSecondsRealtime(Random.Range(0.01f, 0.2f));
+        }
+    }
+}
---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (SceneManager.GetActiveScene().name != sceneName)
+        {
+            if (collision.TryGetComponent<Player>(out Player player))
+            {
+                PlayerPrefs.SetString("Scene", sceneName);
+                SceneManager.LoadScene("LoadScreen");
+            }
+        }
+    }
+}

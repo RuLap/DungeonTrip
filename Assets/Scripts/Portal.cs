9830c769cb44e627665af502e38036b306fd01f9
Rust555 <rustamgafiyatov@mail.ru>
2020-12-01 22:03:18 +0500

    Правки к порталу

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -5,21 +5,33 @@ using UnityEngine.SceneManagement;
 public class Portal : MonoBehaviour
 {
     [SerializeField]
-    private string sceneName;
+    private string nextSceneName;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (SceneManager.GetActiveScene().name != sceneName)
+        string sceneName = SceneManager.GetActiveScene().name;
+        if (sceneName != nextSceneName)
         {
             if (collision.TryGetComponent<Player>(out Player player))
             {
-                string sceneName = SceneManager.GetActiveScene().name;
-                if (sceneName.Contains("Boss"))
+                if(SaveSystem.Info.name == "Boss6")
                 {
+                    SaveSystem.SaveGame();
+                    PlayerPrefs.SetString("Scene", "Ending");
+                    SceneManager.LoadScene("LoadScreen");
+                    return;
+                }
+                if (!nextSceneName.Contains("Boss"))
+                {
+                    SaveSystem.Info.name = "Level";
                     SaveSystem.Info.Level++;
                 }
+                else
+                {
+                    SaveSystem.Info.name = "Boss";
+                }
                 SaveSystem.SaveGame();
-                PlayerPrefs.SetString("Scene", sceneName);
+                PlayerPrefs.SetString("Scene", nextSceneName);
                 SceneManager.LoadScene("LoadScreen");
             }
         }

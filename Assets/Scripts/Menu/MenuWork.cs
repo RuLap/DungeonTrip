3cbff64fa3692c02f38848d752ac50f8fd984016
MilkissSun <55906810+MilkissSun@users.noreply.github.com>
2020-12-05 12:46:18 +0500

    добавлено меню и тест на меню

--- Assets/Scripts/Menu/MenuWork.cs
+++ Assets/Scripts/Menu/MenuWork.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using System.Linq;
 
 public class MenuWork : MonoBehaviour
 {
-    private void Start()
-    {
-    }
+    public int quality = 2; //Качество
+    public bool isFullScreen = true; //Полноэкранный режим
+    public AudioMixer AudioMixer; //Регулятор громкости
+    public Dropdown resolutionDropdown; //Список с разрешениями для игры
+    public Dropdown qualityDropdown;//Список с уровнями качества
+
+    public bool isOpened = false; //Открыто ли внутриигровое меню
+
     public void ContinuePressed()
     {
         PlayerPrefs.SetString("PlayerJson", "PlayerXP");
@@ -26,5 +34,64 @@ public class MenuWork : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Exit pressed!");
+    }
+    //методы изменения настроек
+    public void ChangeVolume(float val)
+    {
+        //volume = val;
+        AudioMixer.SetFloat("MasterVolume", val);
+    }
+    public void ChangeResolution()
+    {
+        //currResolutionIndex = index;
+        if (resolutionDropdown.value == 0)
+        {
+            Screen.SetResolution(1366, 768, true);
+        }
+        if (resolutionDropdown.value == 1)
+        {
+            Screen.SetResolution(1080, 720, true);
+        }
+        if (resolutionDropdown.value == 2)
+        {
+            Screen.SetResolution(1024, 768, true);
+        }
+        if (resolutionDropdown.value == 3)
+        {
+            Screen.SetResolution(1440, 900, true);
+        }
+    }
+    public void ChangeFullScreenMode()
+    {
+        //isFullScreen = val;
+        isFullScreen = !isFullScreen;
+        Screen.fullScreen = isFullScreen;
+        isFullScreen = false;
+    }
+    public void ChangeQuality(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);
+        //quality = index;
+    }
+    //методы внутриигрового меню
+
+    public void ShowHideMenu()
+    {
+        isOpened = !isOpened;
+        GetComponent<Canvas>().enabled = isOpened;  //Включение или отключение Canvas. Ещё тут можно использовать метод SetActive()
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public static void EnterMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void ContinueGame()
+    {
+
     }
 }
---
+++ Assets/Tests/MenuWorkTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class MenuWorkTest: MonoBehaviour
+    {
+        private void Start()
+        {
+            var menu = GameObject.FindObjectOfType<MenuWork>();
+            SceneManager.sceneLoaded += EnterMainMenu;
+        }
+        // A Test behaves as an ordinary method
+        [Test]
+        public void MenuWorkChangeFullScreenTest()
+        {
+            var menu = GameObject.FindObjectOfType<MenuWork>();
+            bool isFull = true;
+            
+            menu.ChangeFullScreenMode();
+            Assert.IsTrue(isFull != GameObject.FindObjectOfType<MenuWork>().isFullScreen);
+        }
+
+        [Test]
+        public void MenuWorkEnterMainMenuTest()
+        {
+            
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator MenuWorkTestWithEnumeratorPasses()
+        {
+            var menu = GameObject.FindObjectOfType<MenuWork>();
+            //var scene = GameObject.FindGameObjectsWithTag("MainMenu");
+            menu.EnterMainMenu();
+            yield return new WaitForSecondsRealtime(1f);
+            Assert.IsTrue(SceneManager.GetActiveScene().name == "MainMenu");
+        }
+    }
+}

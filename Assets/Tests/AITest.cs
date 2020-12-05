21473e206fe01fc8f7d8fa566eabf11012e46731
AdmiralSlowpoke <maksimzlt@gmail.com>
2020-12-05 13:42:31 +0500

    Тесты

---
+++ Assets/Tests/AITest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class AITest
+    {
+        [UnityTest]
+        public async void IsAgressive()
+        {
+            SceneManager.LoadSceneAsync("Boss1");
+            //await Assert.IsTrue(SceneManager.GetActiveScene().isLoaded);
+
+        }
+    }
+}
